c8101ff4c8375285ea96eab26643a009155a7dac
Dylan <1171357812@qq.com>
2020-05-03 16:08:52 +0800

    添加gateway服务

---
+++ gateway/src/test/java/com/dylan/mylog/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dylan.mylog.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
