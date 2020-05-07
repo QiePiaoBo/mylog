0acce98cc8907a881b813f180bc5faef7172ce3a
Dylan <1171357812@qq.com>
2020-05-07 22:12:50 +0800

    添加配置中心模块

---
+++ config/src/test/java/com/dylan/mylog/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dylan.mylog.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
