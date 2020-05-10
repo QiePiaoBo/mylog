3c6f37cdf39a1b383ba900c0940d399d3e3eb0c3
Dylan <1171357812@qq.com>
2020-05-10 11:08:38 +0800

    框架大修04

--- platform/config/src/main/java/com/dylan/mylog/config/ConfigApplication.java
+++ platform/config/src/main/java/com/dylan/mylog/config/ConfigApplication.java
@@ -1,14 +1,10 @@
 package com.dylan.mylog.config;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.context.ConfigurableApplicationContext;
-import java.util.concurrent.TimeUnit;
-
 
 @EnableDiscoveryClient
 @EnableConfigServer
