02243cbfde5245950ec78bb18f636dec9dda04f5
Dylan <1171357812@qq.com>
2020-05-11 18:52:10 +0800

    尝试将ribbon配置nacos

--- demos/dm-ribbon/src/main/java/com/mylog/demos/dm_ribbon/RibbonApplication.java
+++ demos/dm-ribbon/src/main/java/com/mylog/demos/dm_ribbon/RibbonApplication.java
@@ -2,10 +2,10 @@ package com.mylog.demos.dm_ribbon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableEurekaClient
 @SpringBootApplication
+@EnableDiscoveryClient
 public class RibbonApplication {
 
     public static void main(String[] args) {
---
+++ demos/dm-ribbon/src/main/java/com/mylog/demos/dm_ribbon/controller/MeetngController.java
@@ -0,0 +1,19 @@
+package com.mylog.demos.dm_ribbon.controller;
+
+import com.mylog.demos.dm_ribbon.service.MeetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MeetngController {
+
+    @Autowired
+    MeetingService meetingService;
+
+
+    @RequestMapping("/who")
+    public String whoIsThere(){
+        return meetingService.getPerson();
+    }
+}
