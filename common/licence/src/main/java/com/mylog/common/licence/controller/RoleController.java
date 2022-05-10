29cb0471315733505c16a77ea9a033807bd81da5
Dylan <1171357812@qq.com>
2022-05-10 22:59:49 +0800

    feat: 添加role的 根据id删除、根据id修改、根据id获取等接口

--- common/licence/src/main/java/com/mylog/common/licence/controller/RoleController.java
+++ common/licence/src/main/java/com/mylog/common/licence/controller/RoleController.java
@@ -21,15 +21,50 @@ public class RoleController {
     @Resource
     private IRoleService roleService;
 
+    /**
+     * 分页获取角色
+     * @param page
+     * @param limit
+     * @return
+     */
     @GetMapping
     public HttpResult getRoleList(@Param("page") Integer page, @Param("limit") Integer limit){
         MyPage myPage = new MyPage(page, limit);
         return roleService.selectRoleList(myPage);
     }
 
+    /**
+     * 创建角色
+     * @param roleDTO
+     * @return
+     */
     @PostMapping
     public HttpResult create(@RequestBody RoleDTO roleDTO){
         return roleService.create(roleDTO);
+    }
+
+    /**
+     * 根据id获取角色
+     */
+    @GetMapping("/{id:\\d+}")
+    public HttpResult getById(@PathVariable Integer id){
+        return roleService.getById(id);
+    }
+
+    /**
+     * 根据id删除角色
+     */
+    @DeleteMapping("/{id:\\d+}")
+    public HttpResult deleteById(@PathVariable Integer id){
+        return roleService.deleteById(id);
+    }
+
+    /**
+     * 根据id修改角色
+     */
+    @PatchMapping
+    public HttpResult updateById(@RequestBody RoleDTO roleDTO){
+        return roleService.updateById(roleDTO);
     }
 
 }
