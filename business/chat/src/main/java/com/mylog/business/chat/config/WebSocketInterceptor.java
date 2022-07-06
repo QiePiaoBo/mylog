63a140719d49847a292ee7e791a3afcbd5c9bd8e
duanqilun <duanqilun@mail.jiehun.com>
2022-07-06 18:33:23 +0800

    feat: 添加websocket功能 待添加websocket的鉴权

---
+++ business/chat/src/main/java/com/mylog/business/chat/config/WebSocketInterceptor.java
@@ -0,0 +1,67 @@
+package com.mylog.business.chat.config;
+
+import com.dylan.logger.MyLogger;
+import com.dylan.logger.MyLoggerFactory;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.http.server.ServletServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.server.HandshakeInterceptor;
+
+import java.util.Map;
+
+/**
+ * @Classname WebSocketInterceptor
+ * @Description WebSocketInterceptor
+ * @Date 7/6/2022 3:52 PM
+ */
+@Component
+public class WebSocketInterceptor implements HandshakeInterceptor {
+
+    private static final MyLogger logger = MyLoggerFactory.getLogger(WebSocketInterceptor.class);
+
+    /**
+     * 握手前
+     * @param request
+     * @param response
+     * @param webSocketHandler
+     * @param map
+     * @return
+     * @throws Exception
+     */
+    @Override
+    public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler webSocketHandler, Map<String, Object> map) throws Exception {
+        ServletServerHttpRequest serverHttpRequest = (ServletServerHttpRequest) request;
+        ServletServerHttpResponse serverHttpResponse = (ServletServerHttpResponse) response;
+        // 参数传入userId和pwd方式
+        // String userId = serverHttpRequest.getServletRequest().getParameter("userId");
+        // String pwd = serverHttpRequest.getServletRequest().getParameter("pwd");
+        // header中
+        String authorization = serverHttpRequest.getServletRequest().getHeader("logicer-chat-protocol");
+        // todo 根据获取到的authorization的值进行鉴权
+        if (StringUtils.isBlank(authorization) || !authorization.equals("hello-world")){
+            return false;
+        }
+        serverHttpResponse.getServletResponse().setHeader("logicer-chat-protocol", authorization);
+        logger.info("start shaking hands >>>>>>");
+
+        return true;
+    }
+
+    /**
+     * 握手后
+     * @param request
+     * @param response
+     * @param webSocketHandler
+     * @param e
+     */
+    @Override
+    public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler webSocketHandler, Exception e) {
+        logger.info("handshake success.");
+    }
+
+
+}
---
+++ business/chat/src/main/java/com/mylog/business/chat/ws/MyWebSocketHandler.java
@@ -0,0 +1,125 @@
+package com.mylog.business.chat.ws;
+
+import com.dylan.logger.MyLogger;
+import com.dylan.logger.MyLoggerFactory;
+import com.mylog.business.chat.config.WebSocketInterceptor;
+import com.mylog.tools.model.model.exception.MyException;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @Classname MyWebSocketHandler
+ * @Description MyWebSocketHandler
+ * @Date 7/6/2022 3:55 PM
+ */
+public class MyWebSocketHandler extends TextWebSocketHandler {
+
+    private static final MyLogger logger = MyLoggerFactory.getLogger(MyWebSocketHandler.class);
+
+    private static final AtomicInteger onlineNum = new AtomicInteger();
+
+    private static final ConcurrentHashMap<String, WebSocketSession> sessionPool = new ConcurrentHashMap<>();
+
+    /**
+     * 在线人数+1
+     */
+    public static void addOnlineCount(){
+        onlineNum.incrementAndGet();
+    }
+
+    /**
+     * 在线人数减一
+     */
+    public static void subOnlineCount(){
+        onlineNum.decrementAndGet();
+    }
+
+
+    /**
+     * 接受客户端消息
+     * @param session
+     * @param message
+     * @throws Exception
+     */
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        // todo 将接收到的报文转化成logicer报文发送给
+        session.sendMessage(new TextMessage(
+                String.format("Got message %s from %s", message.getPayload(), session.getId()))
+        );
+    }
+
+    /**
+     * 连接建立时
+     * @param session
+     * @throws Exception
+     */
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        WebSocketSession put = sessionPool.put(session.getId(), session);
+        if (Objects.isNull(put)){
+            addOnlineCount();
+        }
+    }
+
+    /**
+     * 连接关闭后
+     * @param session
+     * @param status
+     * @throws Exception
+     */
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        String sessionId = session.getId();
+        if (sessionPool.containsKey(sessionId)){
+            sessionPool.remove(sessionId);
+            subOnlineCount();
+        }
+        logger.info("{} disconnect!", sessionId);
+    }
+
+    /**
+     * 群发消息
+     * @param message
+     */
+    public static void sendTopic(String message){
+        if (sessionPool.isEmpty()){
+            return;
+        }
+        for (Map.Entry<String, WebSocketSession> entry : sessionPool.entrySet()){
+            try {
+                entry.getValue().sendMessage(new TextMessage(message));
+            }catch (IOException e){
+                throw new MyException(e);
+            }
+        }
+    }
+
+    /**
+     * 点对点发送
+     * @param uid
+     * @param message
+     */
+    public static void sendToUser(String uid, String message){
+        WebSocketSession socketSession = sessionPool.getOrDefault(uid, null);
+        if (Objects.isNull(socketSession)){
+            return;
+        }
+        try {
+            socketSession.sendMessage(new TextMessage(message));
+        } catch (IOException e) {
+            throw new MyException(e);
+        }
+
+    }
+
+
+}
