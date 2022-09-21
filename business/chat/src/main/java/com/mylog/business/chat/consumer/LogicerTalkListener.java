206b52b2019cda92582172053401e7838459c78a
duanqilun <duanqilun@mail.jiehun.com>
2022-09-21 14:13:30 +0800

    feat: logicer-base已重命名类名

--- business/chat/src/main/java/com/mylog/business/chat/consumer/LogicerTalkListener.java
+++ business/chat/src/main/java/com/mylog/business/chat/consumer/LogicerTalkListener.java
@@ -2,7 +2,8 @@ package com.mylog.business.chat.consumer;
 
 import com.dylan.logger.MyLogger;
 import com.dylan.logger.MyLoggerFactory;
-import com.dylan.mq.RabbitMqConstant;
+import com.dylan.mq.LogicerTalkMqConstant;
+import com.dylan.mq.LogicerTalkMqConstant;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
@@ -16,7 +17,7 @@ public class LogicerTalkListener {
 
     private final MyLogger logger = MyLoggerFactory.getLogger(LogicerTalkListener.class);
 
-    @RabbitListener(queues = {RabbitMqConstant.LOGICER_QUEUE_TALK})
+    @RabbitListener(queues = {LogicerTalkMqConstant.LOGICER_QUEUE_TALK})
     public void receiveMsg(String msg){
         // todo 添加TALK类型消息的处理逻辑（入库）
         logger.info("class:{},message:{}", "LogicerTalkListener", msg);
