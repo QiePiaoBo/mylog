7f320cdc09ba69420c7171668dca82b51baf6991
Dylan <1171357812@qq.com>
2022-12-17 23:56:19 +0800

    feat: 自定义logback的MessageConverter 在打印json格式的日志到文件时，将日志字符串双引号前添加一个转义符 以免json解析失败

---
+++ tools/utils-server/src/main/java/com/mylog/tools/utils/logconverter/LogJsonConverter.java
@@ -0,0 +1,33 @@
+package com.mylog.tools.utils.logconverter;
+
+import ch.qos.logback.classic.pattern.MessageConverter;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import org.slf4j.helpers.MessageFormatter;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.stream.Stream;
+
+/**
+ * @author Dylan
+ * @Description LogJsonConverter
+ * @Date : 2022/12/17 - 22:53
+ */
+@Configuration
+public class LogJsonConverter extends MessageConverter {
+
+    @Override
+    public String convert(ILoggingEvent event) {
+        try {
+            // 将日志中的双引号改为 \"
+            String message = event.getMessage();
+            String replacedVar = message;
+            if (message.contains("\"")){
+                replacedVar = message.replaceAll("\"", "\\\\\"");
+            }
+            String res = MessageFormatter.arrayFormat(replacedVar, event.getArgumentArray()).getMessage();
+            return res;
+        }catch (Exception e){
+            return e.getMessage();
+        }
+    }
+}
