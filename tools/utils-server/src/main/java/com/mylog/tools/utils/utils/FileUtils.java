efbd847c2fcb4ba3d4f8c4855f40b638e1ca05f5
duanqilun <duanqilun@mail.jiehun.com>
2023-06-18 16:09:56 +0800

    feat: 文件转换接口改动

--- tools/utils-server/src/main/java/com/mylog/tools/utils/utils/FileUtils.java
+++ tools/utils-server/src/main/java/com/mylog/tools/utils/utils/FileUtils.java
@@ -1,9 +1,14 @@
 package com.mylog.tools.utils.utils;
 
 
+import io.micrometer.core.instrument.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Objects;
 
 /**
@@ -17,12 +22,12 @@ public class FileUtils {
     public static File multi2File(MultipartFile multi) throws IOException {
 
         File f = null;
-        if(multi.equals("")||multi.getSize()<=0){
+        if (Objects.isNull(multi) || StringUtils.isBlank(multi.getOriginalFilename()) || multi.getSize() <= 0) {
             multi = null;
             return null;
-        }else{
+        } else {
             InputStream ins = multi.getInputStream();
-            f=new File(Objects.requireNonNull(multi.getOriginalFilename()));
+            f = new File(multi.getOriginalFilename());
             inputStreamToFile(ins, f);
             return f;
         }
